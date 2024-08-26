c19b6951c4f1fb10c7a66494f26947bcb1664688
DanTheDan64 <danobrien.bc@gmail.com>
2024-08-26 16:20:07 +1200

    grapple bug fixed next commit

--- Titan Trackers/player_3.gd
+++ Titan Trackers/player_3.gd
@@ -13,9 +13,14 @@ var shoot_to = Vector3.ZERO
 @onready var cam = $Camera3D
 var sens = 0.2
 
+@onready var input_dir = Input.get_vector("left", "right", "up", "down")
+@onready var direction = (cam.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+@onready var direction_flat = ($Marker3D.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+
 
 func _ready():
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+	
 
 
 func _input(event):
@@ -27,6 +32,12 @@ func _input(event):
 	elif InputEvent:
 		if Input.is_action_pressed("escape"):
 			get_tree().quit()
+		elif Input.is_action_just_pressed("jump"):
+			if not is_on_floor():
+				if direction:
+					velocity += direction * 250
+				else:
+					velocity += -cam.transform.basis.z * 250
 
 
 func _physics_process(delta):
@@ -37,19 +48,14 @@ func _physics_process(delta):
 	else:
 		accell = 90
 	
-	var input_dir = Input.get_vector("left", "right", "up", "down")
-	var direction = (cam.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	var direction_flat = ($Marker3D.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	input_dir = Input.get_vector("left", "right", "up", "down")
+	direction = (cam.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	direction_flat = ($Marker3D.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	
 	#jump/boost
 	if Input.is_action_just_pressed("jump"):
 		if is_on_floor():
 			velocity.y = jump_velocity
-		else:
-			if direction:
-				velocity += direction * 1000
-			else:
-				velocity += -cam.transform.basis.z * 1000
 	
 	match state:
 		"grappling": grappling(delta)
@@ -71,7 +77,6 @@ func moving(delta, direction, direction_flat):
 	velocity.z = move_toward(velocity.z, direction_flat.z * max_speed, accell * delta)
 	
 	
-	
 	#shoot grapple, go to grapple state
 	if Input.is_action_just_pressed("fire_hook"):
 		var space_state = get_world_3d().direct_space_state
@@ -79,20 +84,21 @@ func moving(delta, direction, direction_flat):
 		
 		var query = PhysicsRayQueryParameters3D.create(
 		cam.global_position,
-		-cam.transform.basis.z * 10000)
+		-cam.transform.basis.z * 4000)
 		var result = space_state.intersect_ray(query)
 		
 		if not result.is_empty():
 			shoot_to = result.position
-			gravity = 30
 			state = "grappling"
+			var aa = preload("res://erth.tscn").instantiate()
+			aa.position = result.position
+			get_parent().add_child(aa)
 			return
 
 
 func grappling(delta):
-	velocity += (shoot_to - position).normalized() * delta * 250
+	velocity += (shoot_to - position).normalized() * delta * 150
 	if Input.is_action_just_released("fire_hook"):
-		gravity = 12
 		state = "moving"
 
 
