b926bee644c6265278dca692b2371ed955394b85
Sheep26 <crabeykiller@gmail.com>
2024-09-15 13:35:11 +1200

    is it meant to return

--- Menu/Options/Video/nsync_display_button.gd
+++ Menu/Options/Video/nsync_display_button.gd
@@ -5,7 +5,9 @@ extends Control
 func _on_check_button_toggled(toggled_on):
 	if toggled_on:
 		#DisplayServer.window_set_vsync_mode(DisplayServer.VSYNC_ENABLED)
+		#return DisplayServer.VSYNC_ENABLED
 		DisplayServer.VSYNC_ENABLED
 	else:
 		#DisplayServer.window_set_vsync_mode(DisplayServer.VSYNC_DISABLED)
+		#return DisplayServer.VSYNC_DISABLED
 		DisplayServer.VSYNC_DISABLED
